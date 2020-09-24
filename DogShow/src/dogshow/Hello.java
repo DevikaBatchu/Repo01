ef93c33def8e92a11838a3b829443f63ef9f5115
devika <devika@DESKTOP-SBJMBLQ>
2020-09-24 20:32:53 +0530

    DogShow project added

---
+++ DogShow/src/dogshow/Hello.java
@@ -0,0 +1,30 @@
+package dogshow;
+
+import javax.inject.Inject;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+
+public class Hello extends AbstractHandler{
+	
+	public static final String ID = "dogshow.handler";
+	
+	@Inject IWorkbench workbench;
+	
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		// TODO Auto-generated method stub
+		
+		MessageDialog.openInformation(HandlerUtil.getActiveWorkbenchWindow(event).getShell(), 
+									"Welcome", "Hello ......  Welcome!!!");
+		
+		return null;
+	}
+
+}
