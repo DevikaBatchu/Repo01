0558ac59d3681c67186958aa42deca995f0f6c81
devika <devika@DESKTOP-SBJMBLQ>
2020-09-24 20:42:01 +0530

    Merge project added

---
+++ MergePlugins/src/mergeplugins/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,32 @@
+package mergeplugins;
+
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+	private static final String PERSPECTIVE_ID = "MergePlugins.perspective"; //$NON-NLS-1$
+
+	@Override
+    public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
+        return new ApplicationWorkbenchWindowAdvisor(configurer);
+    }
+    
+    @Override
+	public String getInitialWindowPerspectiveId() {
+		return PERSPECTIVE_ID;
+	}
+    
+    public boolean preShutdown() {
+		
+		Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+		String dialogBoxTitle = "Confirm Exit";
+		String Question = "are you sure want to exit the application ?";
+		return MessageDialog.openQuestion(shell, dialogBoxTitle, Question);
+	}
+	
+}
